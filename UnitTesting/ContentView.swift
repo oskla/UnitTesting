a5a6cf4a86c464f78adc2d693ca032fd7f0e1025
oskla <oskarlarssson@gmail.com>
2022-11-02 09:15:22 +0100

    Initial Commit

---
+++ UnitTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitTesting
+//
+//  Created by Oskar Larsson on 2022-11-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitTesting/UnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingApp.swift
+//  UnitTesting
+//
+//  Created by Oskar Larsson on 2022-11-02.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
